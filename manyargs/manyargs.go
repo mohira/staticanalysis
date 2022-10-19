a4adc3608a46add924fd7bdd20e484cc58d84e88
mohira <shukatsudaison@gmail.com>
2022-10-19 16:55:36 +0900

    green: manyargs

--- manyargs/manyargs.go
+++ manyargs/manyargs.go
@@ -1,16 +1,38 @@
 package manyargs
 
 import (
+	"go/ast"
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var Analyzer = &analysis.Analyzer{
 	Name:     "manyargs",
 	Doc:      "引数が多い関数を見つけるよ",
 	Run:      run,
-	Requires: nil,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
+var criteria = 5
+
 func run(pass *analysis.Pass) (any, error) {
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.FuncType)(nil),
+	}
+
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		f := n.(*ast.FuncType)
+
+		if isTooManyArgs(f) {
+			pass.Reportf(f.Pos(), "引数が多いな！")
+		}
+	})
 	return nil, nil
+}
+
+func isTooManyArgs(f *ast.FuncType) bool {
+	return f.Params.NumFields() >= criteria
 }
