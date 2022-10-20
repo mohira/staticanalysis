a24d60e0916da000c568e5e144f569b87c25f2ca
mohira <shukatsudaison@gmail.com>
2022-10-20 23:32:06 +0900

    feat: 名前無しの引数だけで構成された関数を発見できるようにした。

--- unnamedfunc/unnamedfunc.go
+++ unnamedfunc/unnamedfunc.go
@@ -8,31 +8,35 @@ import (
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-const doc = "unnamedfunc is ..."
-
-// Analyzer is ...
 var Analyzer = &analysis.Analyzer{
-	Name: "unnamedfunc",
-	Doc:  doc,
-	Run:  run,
-	Requires: []*analysis.Analyzer{
-		inspect.Analyzer,
-	},
+	Name:     "unnamedfunc",
+	Doc:      "名前無し引数を使っているパッケージ関数を探してその使いみちを知る",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func run(pass *analysis.Pass) (any, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
+		(*ast.FuncDecl)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+		f := n.(*ast.FuncDecl)
+
+		if f.Recv != nil {
+			return
+		}
+
+		var isUnnamed bool
+		for _, p := range f.Type.Params.List {
+			if p.Names == nil {
+				isUnnamed = true
 			}
+		}
+		if isUnnamed {
+			pass.Reportf(f.Pos(), "発見！ 名前無し引数を使ったパッケージ関数")
 		}
 	})
 
